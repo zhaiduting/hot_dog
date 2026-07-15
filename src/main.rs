a5d1deff125d8378f7826ba30c82b5eacb232a85
zhaiduting <zhaiduting@163.com>
2026-07-15 12:45:36 +0800

    组件拆分

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,31 @@ fn main() {
 fn App() -> Element {
     rsx! {
         document::Link { rel: "stylesheet", href: MAIN_CSS }
+        Title {}
+        DogView {}
+    }
+}
+
+#[component]
+fn Title() -> Element {
+    rsx! {
         div { id: "title",
             h1 { "HotDog! 🌭" }
         }
+    }
+}
+
+#[component]
+fn DogView() -> Element {
+    let skip = move |_| info!("Skipped!");
+    let save = move |_| info!("saved!");
+    rsx! {
         div { id: "dogview",
             img { src: "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg" }
         }
         div { id: "buttons",
-            button { id: "skip", "skip" }
-            button { id: "save", "save!" }
+            button { onclick: skip, id: "skip", "skip" }
+            button { onclick: save, id: "save", "save!" }
         }
     }
 }
