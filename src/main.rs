ca578838e8e13c76c6eea8a08595175994780e4d
zhaiduting <zhaiduting@163.com>
2026-07-17 15:26:08 +0800

    使用 response.text() 实现 save()

--- src/main.rs
+++ src/main.rs
@@ -24,15 +24,29 @@ fn Title() -> Element {
 
 #[component]
 fn DogView() -> Element {
+    let mut img_src = use_signal(|| "".to_string());
+    // 改用 use_signal(|| "") 会导致生命周期问题
+    
     let skip = move |_| info!("Skipped!");
-    let save = move |_| info!("saved!");
+    let save = move |_| async move {
+        let response = reqwest::get("https://dog.ceo/api/breeds/image/random").await.unwrap();
+        let text = response.text().await.unwrap();
+        let dogApi: DogApi = serde_json::from_str(&text).unwrap();
+        img_src.set(dogApi.message);
+    };
     rsx! {
         div { id: "dogview",
-            img { src: "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg" }
+            img { src: {img_src} }
         }
         div { id: "buttons",
             button { onclick: skip, id: "skip", "skip" }
             button { onclick: save, id: "save", "save!" }
         }
     }
+}
+
+use serde::Deserialize;
+#[derive(Deserialize)]
+struct DogApi {
+    message: String,
 }
